f16ef49a33f3781b2408c270bcc7aed9dbd97768
♪ hiljusti 🎮 <hiljusti@amazon.com>
2021-01-12 23:26:58 -0800

    Update a C enum

--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -2,16 +2,16 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-      BLACK  = 0,
-      BROWN  = 1,
-      RED    = 2,
-      ORANGE = 3,
-      YELLOW = 4,
-      GREEN  = 5,
-      BLUE   = 6,
-      VIOLET = 7,
-      GREY   = 8,
-      WHITE  = 9
+      BLACK,
+      BROWN,
+      RED,
+      ORANGE,
+      YELLOW,
+      GREEN,
+      BLUE,
+      VIOLET,
+      GREY,
+      WHITE
 } resistor_band_t;
 
 int color_code(resistor_band_t color);
