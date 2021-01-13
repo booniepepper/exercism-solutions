b7e41321dc543b3d91609964d9055fbb2777fe44
♪ hiljusti 🎮 <hiljusti@amazon.com>
2021-01-12 23:12:08 -0800

    Update Python raindrop solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,9 +1,10 @@
-sounds = {
-    'Pling': 3,
-    'Plang': 5,
-    'Plong': 7
-}
+sounds = [
+    ['Pling', 3],
+    ['Plang', 5],
+    ['Plong', 7]
+]
 
 def convert(n):
-    sound = ''.join(s for s, m in sounds.items() if n % m == 0)
+    sound = ''.join(s for s, m in sounds if n % m == 0)
     return sound or str(n)
+
