e30cff77b8db986812327d06a8b1d9458a946d7f
♪ hiljusti 🎮 <hiljusti@amazon.com>
2021-01-15 00:38:54 -0800

    Simplify haskell rna solution

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,15 +1,13 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
 module DNA (toRNA) where
 
-class DNA err ok where
-    toRNA :: ok -> Either err ok
-instance DNA Char Char where
-    toRNA n = case n of
-        'G' -> Right 'C'
-        'C' -> Right 'G'
-        'T' -> Right 'A'
-        'A' -> Right 'U'
-        _ -> Left n
-instance DNA Char String where
-    toRNA = mapM toRNA
+toComplement :: Char -> Either Char Char
+toComplement n = case n of
+    'G' -> Right 'C'
+    'C' -> Right 'G'
+    'T' -> Right 'A'
+    'A' -> Right 'U'
+    _ -> Left n
+
+toRNA :: String -> Either Char String
+toRNA = mapM toComplement
 
