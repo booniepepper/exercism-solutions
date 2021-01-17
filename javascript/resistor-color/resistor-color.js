b6b8968d8b7f0a256e12168e3dbec717a69a59ab
♪ hiljusti 🎮 <hiljusti@amazon.com>
2021-01-16 22:44:24 -0800

    resist colors in javascript

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,15 @@
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+
+export const colorCode = c => COLORS.indexOf(c);
+
