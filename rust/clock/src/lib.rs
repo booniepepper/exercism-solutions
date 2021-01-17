0ecfb7dda2f0a6100641d070f33d7cb95fd20d2e
♪ hiljusti 🎮 <hiljusti@amazon.com>
2021-01-17 00:41:19 -0800

    make rusty clock more consise

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::{Duration, NaiveTime, Timelike};
+use chrono::{Duration, NaiveTime};
 use std::fmt;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -8,11 +8,10 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let (offset_hours, minutes) = normalize(minutes, (0, 60));
-        let (_, hours) = normalize(hours + offset_hours, (0, 24));
-        Clock {
-            time: NaiveTime::from_hms(hours as u32, minutes as u32, 0),
-        }
+        let time = NaiveTime::from_hms(0, 0, 0)
+            + Duration::hours(hours as i64)
+            + Duration::minutes(minutes as i64);
+        Clock { time }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
@@ -22,22 +21,8 @@ impl Clock {
 }
 
 impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:#02}:{:#02}", self.time.hour(), self.time.minute())
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(&self.time.format("%H:%M").to_string())
     }
 }
 
-fn normalize(n: i32, bounds: (i32, i32)) -> (i32, i32) {
-    let (lower, upper) = bounds;
-    let mut carry = 0;
-    let mut n = n;
-    while n < lower {
-        n += upper;
-        carry -= 1;
-    }
-    while n >= upper {
-        n -= upper;
-        carry += 1;
-    }
-    (carry, n)
-}
