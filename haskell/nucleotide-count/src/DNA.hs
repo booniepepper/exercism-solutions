9885ff28e2974ac7467cdb1c8d24bdf46797e887
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-04-15 23:33:42 -0700

    Solve Haskell nucleotide problem

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,14 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map)
+import Control.Monad (foldM)
+import Data.Map (Map, alter, empty)
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Read, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts = foldM count empty . map (\c -> readMaybe [c]) where
+  count _ Nothing = Left "error"
+  count acc (Just n) = Right $ alter (Just . succ . fromMaybe 0) n acc
+
