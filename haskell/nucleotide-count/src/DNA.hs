a18c21aa52406032ba2b7b651ebde3a359539fbd
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-04-18 00:31:40 -0700

    Be less 'maybe' about Haskell

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -2,13 +2,17 @@ module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Control.Monad (foldM)
 import Data.Map (Map, alter, empty)
-import Data.Maybe (fromMaybe)
-import Text.Read (readMaybe)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Read, Show)
+type Counts = Map Nucleotide Int
 
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts = foldM count empty . map (\c -> readMaybe [c]) where
-  count _ Nothing = Left "error"
-  count acc (Just n) = Right $ alter (Just . succ . fromMaybe 0) n acc
+fromChar :: Char -> Either String Nucleotide
+fromChar c
+  | c `elem` "ACGT" = Right (read [c])
+  | otherwise = Left "error"
+
+nucleotideCounts :: String -> Either String Counts
+nucleotideCounts = foldM count empty where
+  count :: Counts -> Char -> Either String Counts
+  count acc = fmap (\n -> alter (Just . maybe 1 succ) n acc) . fromChar
 
