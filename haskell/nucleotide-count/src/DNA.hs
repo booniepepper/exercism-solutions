08f1ea725f2c2d42fe282ef4cc4cd8211217ed57
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-04-21 11:00:10 -0700

    Ever simplifying Haskell

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,19 +1,14 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Control.Monad (foldM)
-import Data.Map (Map, alter, empty)
-import Data.Maybe (fromMaybe)
-import Text.Read (readMaybe)
+import Data.Map (Map, insertWith, empty)
+import Text.Read (readEither)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Read, Show)
 type Counts = Map Nucleotide Int
 
-fromChar :: Char -> Either String Nucleotide
-fromChar c = fromMaybe (Left "error") . fmap Right $ readMaybe [c]
-
 nucleotideCounts :: String -> Either String Counts
 nucleotideCounts = foldM count empty where
-  count acc = fmap (countInto acc) . fromChar
-  countInto counts = \n -> alter incrementing n counts
-  incrementing = Just . maybe 1 succ
+  count acc = fmap (countInto acc) . readEither . (:[])
+  countInto counts = \n -> insertWith (+) n 1 counts
 
