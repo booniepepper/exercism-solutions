c4dede64d1c0f503f58dd5675ded75cac1b30120
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-04-24 10:31:44 -0700

    Solve Haskell Luhn problem

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,4 +1,19 @@
 module Luhn (isValid) where
 
+import Control.Arrow ((>>>))
+import Data.Char (isDigit)
+
 isValid :: String -> Bool
-isValid n = error "You need to implement this function."
+isValid str
+  | length (filter isDigit str) > 1 =
+    filter isDigit
+      >>> reverse
+      >>> map (\c -> read [c])
+      >>> zip [1..]
+      >>> map (\(i, n) -> if even i then 2 * n else n)
+      >>> map (\n -> if n > 9 then n - 9 else n)
+      >>> sum
+      >>> (\n -> n `mod` 10 == 0)
+      $ str
+  | otherwise = False
+
