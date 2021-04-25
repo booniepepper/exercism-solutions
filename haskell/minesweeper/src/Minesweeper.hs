47891bbcd4968b83d1d47f9f04843ae45c568dae
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-04-25 03:52:16 -0700

    Solve Haskell minesweeper problem

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -1,4 +1,30 @@
 module Minesweeper (annotate) where
 
-annotate :: [String] -> [String]
-annotate board = error "You need to implement this function."
+import Data.Char (intToDigit)
+
+type Board = [[Char]]
+
+annotate :: Board -> Board
+annotate board = result where
+
+  result = map (map toPiece) coordBoard
+  coordBoard = toCoords board
+
+  toCoords =
+    map (\(row, spaces) -> zip [(row, col) | col <- [1..]] spaces)
+    . zip [1..]
+
+  toPiece (_, '*') = '*'
+  toPiece ((row, col), _) =
+    (\c -> if c == '0' then ' ' else c)
+    . intToDigit
+    . length
+    . filter (elem '*')
+    . map (\(rOff, cOff) -> lookup (row+rOff, col+cOff) coords)
+    $ offsets
+
+  coords = concat coordBoard
+
+  offsets :: [(Int, Int)]
+  offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
