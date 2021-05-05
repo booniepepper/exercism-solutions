ffa2698f23312088b14d7258309844df4863f66e
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-05-04 21:49:19 -0700

    Solve hamming distance in Erlang

--- erlang/hamming/src/hamming.erl
+++ erlang/hamming/src/hamming.erl
@@ -2,5 +2,18 @@
 
 -export([distance/2]).
 
+diff_count(As, Bs) -> diff_count(As, Bs, 0).
+diff_count([], [], N) -> N;
+diff_count([A|As], [B|Bs], N) ->
+  Inc = case A == B of
+          true -> 0;
+          false -> 1
+        end,
+  diff_count(As, Bs, N + Inc).
 
-distance(_Strand1, _Strand2) -> undefined.
+distance(Left, Right) ->
+  case length(Left) =:= length(Right) of
+    true -> diff_count(Left, Right);
+    false -> {error, "left and right strands must be of equal length"}
+  end.
+
