559b638362e4ae9523f0a7ec350edbb6816fbb20
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-05-21 23:50:21 -0700

    Solve all-your-base in Haskell

--- haskell/all-your-base/src/Base.hs
+++ haskell/all-your-base/src/Base.hs
@@ -4,4 +4,13 @@ data Error a = InvalidInputBase | InvalidOutputBase | InvalidDigit a
     deriving (Show, Eq)
 
 rebase :: Integral a => a -> a -> [a] -> Either (Error a) [a]
-rebase inputBase outputBase inputDigits = error "You need to implement this function."
+rebase inputBase outputBase inputDigits
+  | inputBase < 2 = Left InvalidInputBase
+  | outputBase < 2 = Left InvalidOutputBase
+  | length invalidDigits > 0 = Left (InvalidDigit (head invalidDigits))
+  | otherwise = Right (reverse . toBase . fromBase $ inputDigits)
+  where
+    fromBase xs = sum . zipWith (*) (reverse xs) $ [inputBase ^  k | k <- [0..length xs]]
+    toBase 0 = []
+    toBase x = x `mod` outputBase : toBase (x `div` outputBase)
+    invalidDigits = filter (\x -> x < 0 || inputBase <= x) inputDigits
