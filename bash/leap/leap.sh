7788d0d1d3842c7668409ef53a325852627a1bc2
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-06-18 23:16:27 -0700

    Get fancy in Bash

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -2,14 +2,13 @@
 
 year="$1"
 
-if [[ ! "$year" =~ ^[0-9]+$ ]] || [[ -n "$2" ]] ; then
+if [[ ! $year =~ ^[0-9]+$ || -n $2 ]] ; then
     echo 'Usage: leap.sh <year>'
     exit 1
 fi
 
-if (($year % 4 == 0)) && (($year % 100 != 0)) || (($year % 400 == 0)); then
-    echo true
-else
-    echo false
-fi
+(($year % 4 == 0)) \
+    && (($year % 100 != 0)) \
+    || (($year % 400 == 0)) \
+    && echo true || echo false
 
