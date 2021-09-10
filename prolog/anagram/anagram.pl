44ebd673d09596baeb40ba115f9c110dc0a74da3
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-09-10 00:58:18 -0700

    Add an anagrammer in Prolog

--- prolog/anagram/anagram.pl
+++ prolog/anagram/anagram.pl
@@ -1,15 +1,19 @@
+% Why does this fail for unicode anagrams? Is it only the runner? It works on my machine...
+
 anagram(Word, Options, Matching) :-
   include(is_anagram(Word), Options, Matching).
 
 is_anagram(WordA, WordB) :-
-  string_lower(WordA, LowerA),
-  string_lower(WordB, LowerB),
-  \+ same(LowerA, LowerB),
-  sorted_chars(LowerA, Sorted),
-  sorted_chars(LowerB, Sorted).
+  insensitive(WordA, InsensitiveA),
+  insensitive(WordB, InsensitiveB),
+  InsensitiveA \== InsensitiveB,
+  sorted_chars(InsensitiveA, Sorted),
+  sorted_chars(InsensitiveB, Sorted).
 
-same(Same, Same).
+insensitive(Word, Insensitive) :-
+  unicode_map(Word, Insensitive, [casefold]).
 
 sorted_chars(Word, Sorted) :-
   atom_chars(Word, Chars),
   msort(Chars, Sorted).
+
